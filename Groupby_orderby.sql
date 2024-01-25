75bf61daff589a08833dc9fc8a4796ef46f541d2
Cagan Demir <145290776+cagandemirmr@users.noreply.github.com>
2024-01-25 20:01:32 +0300

    Add files via upload

---
+++ Groupby_orderby.sql
@@ -0,0 +1,24 @@
+-- Tabloyu oluştur
+--  CREATE TABLE EmployeeDemographics
+-- (
+--     EmployeeID int,
+--     FirstName varchar(50),
+--     LastName varchar(50),
+--     Age int,
+--     Gender varchar(50)
+-- );
+-- 
+-- -- Veriyi ekle
+-- INSERT INTO EmployeeDemographics VALUES
+-- (1001, 'JIM', 'Halpert', 30, 'MALE');
+SELECT *
+FROM EmployeeDemographics
+--Sadece seçer bir kıyaslama olmaz.
+ORDER BY 4 DESC, 5 DESC
+--sıralamayı direkt colonun ismiyle belirtebileceğimiz gibi colonun numarasıyla da belirleyebiliriz.
+-- İki kolonun sıralama tercihleri virgülle ayrılır.
+SELECT Gender,Age,COUNT(Gender)
+FROM EmployeeDemographics
+GROUP BY Gender
+--Grupla birlikte daha sade bir output alırız.
+
